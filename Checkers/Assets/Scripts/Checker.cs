35228d5097c651bd047d66be2208010de1a666f1
Aizak1 <73393039+Aizak1@users.noreply.github.com>
2021-03-16 18:02:22 +0300

    Changing in Kinhs systems(Not ended)

--- Checkers/Assets/Scripts/Checker.cs
+++ Checkers/Assets/Scripts/Checker.cs
@@ -51,28 +51,24 @@ public class Checker : MonoBehaviour
             List<Checker> checkersBetweenKingPositions = new List<Checker>();
             if (CheckActionCondition(x1, z1, x2, z2, isWhiteturn, Mathf.Abs(x2 - x1)))
             {
-                int nextRawZstart = z1+1;
-                int nextRawZend = z1 + 2;
-                for (int i = x1+1; i < x2; i++)
+                for (int x = x1+2; x < x2; x+=2)
                 {
-                    
-                    for (int j = nextRawZstart; j < nextRawZend; j++)
+                    for (int z = z1+2; z < z2; z+=2)
                     {
-                        if (board[i, j] != null)
+                        Checker checkerToDelete = board[(x1 + x) / 2, (z1 + z) / 2];
+                        if (checkerToDelete != null && checkerToDelete._isWhite != _isWhite)
                         {
-                            checkersBetweenKingPositions.Add(board[i, j]);
-                           
+                            checkersBetweenKingPositions.Add(checkerToDelete);
                         }
+                        x1++;
+                        z1++;
                     }
-                    
-                    nextRawZstart = nextRawZend;
-                    nextRawZend++;
-                   
                 }
                 if (checkersBetweenKingPositions.Count <= 1)
                     return true;
                 else
                     return false;
+                
             }
                 
                 
@@ -80,26 +76,31 @@ public class Checker : MonoBehaviour
         return false;
     }
 
-    private bool CheckActionCondition(int x1, int z1, int x2, int z2, bool isWhiteturn, int conditionDelta)
+    private bool CheckActionCondition(int x1, int z1, int x2, int z2, bool isWhiteTurn, int conditionDelta)
     {
-        return Mathf.Abs(x2 - x1) == conditionDelta && Mathf.Abs(z2 - z1) == conditionDelta && _isWhite == isWhiteturn;
+        return Mathf.Abs(x2 - x1) == conditionDelta && Mathf.Abs(z2 - z1) == conditionDelta && _isWhite == isWhiteTurn;
     }
 
     public bool IsForcedToMove(Checker[,]board,int x1,int z1,bool isWhiteTurn,int beatDelta)
     {
+       
         for (int x = 0; x < board.GetLength(0); x++)
         {
             for (int z = 0; z < board.GetLength(1); z++)
             {
-                if(CheckActionCondition(x1,z1,x,z,isWhiteTurn,beatDelta) && board[x,z] == null)
-                {
+                
+               if (CheckActionCondition(x1, z1, x, z, isWhiteTurn, beatDelta) && board[x, z] == null)
+               {
                     Checker checkerToDelete = board[(x1 + x) / 2, (z1 + z) / 2];
-                    if (checkerToDelete!=null && checkerToDelete.IsWhite != IsWhite)
+                    if (checkerToDelete != null && checkerToDelete.IsWhite != IsWhite)
                     {
-                        return true;
+                      return true;
                     }
-                }
+               }
+
+               
             }
+            
         }
         return false;
     }
