4b24d2cde8c6953d4977d75e787ebdbd4e9b2b51
Aizak1 <73393039+Aizak1@users.noreply.github.com>
2021-03-19 18:50:27 +0300

    Main Menu

--- Checkers/Assets/Scripts/MusicPlayer.cs
+++ Checkers/Assets/Scripts/MusicPlayer.cs
@@ -7,19 +7,33 @@ public class MusicPlayer : MonoBehaviour
     [SerializeField] private AudioClip[] _tracks;
     AudioSource _audioSource;
 
-    
+    private static MusicPlayer _player;
+    private void Awake()
+    {
+        if (_player == null)
+        {
+            _player = this;
+        }
+           
+        else
+       {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+        _audioSource = GetComponent<AudioSource>();
+    }
 
     void Start()
-    {
-        _audioSource = GetComponent<AudioSource>();
-       
+    { 
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(!_audioSource.isPlaying)
-        _audioSource.PlayOneShot(_tracks[Random.Range(0,_tracks.Length)]);
+        if (!_audioSource.isPlaying)
+            _audioSource.PlayOneShot(_tracks[Random.Range(0, _tracks.Length)]);
     }
     
 }
