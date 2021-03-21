8279f835e46e0f67a7203efaaea5758f9b0d0c48
Aizak1 <73393039+Aizak1@users.noreply.github.com>
2021-03-21 21:12:38 +0300

    Stack

--- Checkers/Assets/Scripts/History.cs
+++ Checkers/Assets/Scripts/History.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,20 +7,21 @@ using UnityEngine.UI;
 public class History : MonoBehaviour
 {
     private Text _fieldText;
-   
+    public Stack<Tuple<Vector2Int, Vector2Int,bool>> StepsHistory { get; private set; }
     private void Start()
     {
         _fieldText = GetComponent<Text>();
+        StepsHistory = new Stack<Tuple<Vector2Int, Vector2Int, bool>>();
     }
 
     public void AddRecord(Vector2Int start,Vector2Int final,bool isWhiteTurn)
     {
         string lettersOnBoard = "ABCDEFGH";
+        StepsHistory.Push(Tuple.Create(start, final,isWhiteTurn));
         if (isWhiteTurn)
             _fieldText.text = $" W - {lettersOnBoard[start.y]}{start.x+1} : {lettersOnBoard[final.y]}{final.x+1} \n" +  _fieldText.text;
         else
             _fieldText.text =  $" B - {lettersOnBoard[start.y]}{start.x + 1} : {lettersOnBoard[final.y]}{final.x + 1} \n" + _fieldText.text;
         
-
     }
 }
