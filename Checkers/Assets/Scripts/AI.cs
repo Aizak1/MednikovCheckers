41fd360ac0e185b7e4400529b6aee729d6e2515d
Aizak1 <73393039+Aizak1@users.noreply.github.com>
2021-03-22 18:19:45 +0300

    Last UI changes

--- Checkers/Assets/Scripts/AI.cs
+++ Checkers/Assets/Scripts/AI.cs
@@ -3,6 +3,24 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+public class Move
+{
+    Checker _selectedChecker;
+    Vector2Int _startPosition;
+    Vector2Int _finalPosition;
+
+    public Checker SelectedChecker => _selectedChecker;
+    public Vector2Int StartPosition => _startPosition;
+
+    public Vector2Int FinalPosition => _finalPosition;
+
+    public Move(Checker _selectedChecker, Vector2Int _startPosition, Vector2Int _finalPosition)
+    {
+        this._selectedChecker = _selectedChecker;
+        this._startPosition = _startPosition;
+        this._finalPosition = _finalPosition;
+    }
+}
 
 public class AI : MonoBehaviour
 {
@@ -18,6 +36,7 @@ public class AI : MonoBehaviour
     {
         checkers = FindObjectsOfType<Checker>().ToList();
     }
+
 
  
 }
